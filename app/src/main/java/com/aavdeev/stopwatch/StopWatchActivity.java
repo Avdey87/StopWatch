d3bd7da0a7e5c36e6cc49d07bc0672a283b8466d
avdey <avdey87@gmail.com>
2017-03-09 14:34:22 +0300

    195. onPause, onResume

--- app/src/main/java/com/aavdeev/stopwatch/StopWatchActivity.java
+++ app/src/main/java/com/aavdeev/stopwatch/StopWatchActivity.java
@@ -33,18 +33,34 @@ public class StopWatchActivity extends AppCompatActivity {
 
     }
 
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        wasRunning = running;
+//        running = false;
+//    }
+
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = running;
         running = false;
     }
 
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if (wasRunning){
+//        running = true;}
+//    }
+
+
+
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         if (wasRunning){
-        running = true;}
+            running = true;}
     }
 
     public void onClickStart(View view) {
